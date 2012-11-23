2dc75082291df3c8f2ebfa7fa41519a02e5bc546
Robin Wellner <gyvox.public@gmail.com>
2012-11-23 15:59:44 +0100

    Adds cmvision start script

---
+++ startcmvision.sh
@@ -0,0 +1,5 @@
+rosparam set /cmvision/color_file colors.txt
+rosparam set /cmvision/mean_shift_on false
+rosparam set /cmvision/spatial_radius_pix 0
+rosparam set /cmvision/color_radius_pix 0
+rosrun cmvision cmvision
