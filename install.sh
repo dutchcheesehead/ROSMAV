b0bcb5292e2878f896d299ab81b4ec8310f27c3e
Robin Wellner <me@robinwell.net>
2013-03-22 16:19:24 +0100

    Fixes some things in install script

--- install.sh
+++ install.sh
@@ -1,11 +1,15 @@
+export ROSMAV=`pwd`
+export ROS_PACKAGE_PATH=$ROSMAV:$ROSMAV/cmvision:$ROSMAV/brown-ros-pkg-read-only/experimental:$ROSMAV/stacks:$ROS_PACKAGE_PATH
 pushd brown-ros-pkg-read-only/experimental/ardrone_brown
+echo "Building ardrone_brown"
 rm CMakeCache.txt
 cmake .
 ./build_sdk.sh
 popd
 pushd cmvision
+echo "building cmvision"
 rm CMakeCache.txt
-cmake .
+rosmake cmvision
 popd
 echo export ROSMAV=`pwd` >> ~/.bashrc
 echo export ROS_PACKAGE_PATH=$ROSMAV:$ROSMAV/cmvision:$ROSMAV/brown-ros-pkg-read-only/experimental:$ROSMAV/stacks:$ROS_PACKAGE_PATH >> ~/.bashrc
